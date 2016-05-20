35bd2bb7ef81269abd2bce1a900f39507d5537e8
Aploium <aploium@aploium.com>
2016-05-20 23:38:58 +0800

    fix a path bug when execute the program direct in cmd/bash

--- EasyWebsiteMirror.py
+++ EasyWebsiteMirror.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # coding=utf-8
 import os
-
-os.chdir(os.path.dirname(__file__))
+if os.path.dirname(__file__) != '':
+    os.chdir(os.path.dirname(__file__))
 import traceback
 import pickle
 from datetime import datetime, timedelta
