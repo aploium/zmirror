afe16e72ffe1fab3b8888641c8c7f6521ee143b4
Aploium <i@z.codes>
2016-08-28 22:53:48 +0800

    [-] rollback

--- zmirror/CONSTS.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-import os
-
-__VERSION_TUPLE__ = (0, 27, 1)
-__VERSION__ = ".".join(str(x) for x in __VERSION_TUPLE__)
-__AUTHOR__ = 'Aploium <i@z.codes>'
-__GITHUB_URL__ = 'https://github.com/aploium/zmirror'
-ZMIRROR_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
--- zmirror/zmirror.py
+++ zmirror/zmirror.py
@@ -21,7 +21,11 @@ from urllib.parse import urljoin, urlsplit, urlunsplit, quote_plus
 import urllib.parse
 import requests
 from flask import Flask, request, make_response, Response, redirect
-from . import CONSTS
+from .utils import *
+
+__VERSION__ = '0.27.1'
+__AUTHOR__ = 'Aploium <i@z.codes>'
+__GITHUB_URL__ = 'https://github.com/aploium/zmirror'
 
 try:
     # for python 3.5+ Type Hint
@@ -46,11 +50,6 @@ if "ZMIRROR_UNITTEST" in os.environ:
 else:
     unittest_mode = False
 
-if unittest_mode:
-    import importlib
-    importlib.reload(importlib.import_module("zmirror.utils"))
-from .utils import *
-
 try:  # lru_cache的c语言实现, 比Python内置lru_cache更快
     from fastcache import lru_cache  # lru_cache用于缓存函数的执行结果
 except:
@@ -68,8 +67,8 @@ else:
 from .threadlocal import ZmirrorThreadLocal
 
 if not unittest_mode:  # 在unittest时不输出这几行
-    infoprint('zmirror version: {version} author: {author}'.format(version=CONSTS.__VERSION__, author=CONSTS.__AUTHOR__))
-    infoprint('Github: {site_url}'.format(site_url=CONSTS.__GITHUB_URL__))
+    infoprint('zmirror version: {version} author: {author}'.format(version=__VERSION__, author=__AUTHOR__))
+    infoprint('Github: {site_url}'.format(site_url=__GITHUB_URL__))
 
 try:  # 加载默认设置
     from config_default import *
@@ -171,6 +170,10 @@ connection_pool_per_domain = {}
 if enable_keep_alive_per_domain:
     for _domain in allowed_domains_set:
         connection_pool_per_domain[_domain] = {'session': requests.Session(),}
+
+# 在 cdn_redirect_encode_query_str_into_url 中用于标示编码进url的分隔串
+cdn_url_query_encode_salt = 'zm26'
+_url_salt = re.escape(cdn_url_query_encode_salt)
 
 # ## thread local var ##
 # 与flask的request变量功能类似, 存储了一些解析后的请求信息, 在程序中会经常被调用
@@ -275,6 +278,9 @@ regex_adv_url_rewriter = re.compile(
     flags=re.IGNORECASE
 )
 
+regex_extract_base64_from_embedded_url = re.compile(
+    r'_' + _url_salt + r'(?P<gzip>z?)_\.(?P<b64>[a-zA-Z0-9-_]+=*)\._' + _url_salt + r'_\.[a-zA-Z\d]+\b')
+
 # Response Cookies Rewriter, see response_cookie_rewrite()
 regex_cookie_rewriter = re.compile(r'\bdomain=(\.?([\w-]+\.)+\w+)\b', flags=re.IGNORECASE)
 regex_cookie_path_rewriter = re.compile(r'(?P<prefix>[pP]ath)=(?P<path>[\w\._/-]+?;)')
@@ -320,6 +326,7 @@ app = Flask(  # type: Flask
 
 
 # ########## Begin Utils #############
+
 def encoding_detect(byte_content):
     """
     试图解析并返回二进制串的编码, 如果失败, 则返回 None
@@ -343,6 +350,16 @@ def encoding_detect(byte_content):
         return c_chardet(byte_content)['encoding']
 
     return None
+
+
+@lru_cache(maxsize=1024)
+def s_esc(s):
+    """
+    equivalent to s.replace("/",r"\/")
+    :type s: str
+    :rtype: str
+    """
+    return s.replace("/", r"\/")
 
 
 def cache_clean(is_force_flush=False):
@@ -451,6 +468,49 @@ def cron_task_host():
             traceback.print_exc()
 
 
+# noinspection PyShadowingNames
+def calc_domain_replace_prefix(_domain):
+    """生成各种形式的scheme变体
+    :type _domain: str
+    :rtype: bool
+    """
+    return dict(
+        # normal
+        slash='//' + _domain,
+        http='http://' + _domain,
+        https='https://' + _domain,
+        double_quoted='"%s"' % _domain,
+        single_quoted="'%s'" % _domain,
+        # hex
+        hex_lower=('//' + _domain).replace('/', r'\x2f'),
+        hex_upper=('//' + _domain).replace('/', r'\x2F'),
+        # escape slash
+        slash_esc=s_esc('//' + _domain),
+        http_esc=s_esc('http://' + _domain),
+        https_esc=s_esc('https://' + _domain),
+        double_quoted_esc=r'\"%s\"' % _domain,
+        single_quoted_esc=r"\'%s\'" % _domain,
+        # double escape slash
+        slash_double_esc=('//' + _domain).replace('/', r'\\/'),
+        http_double_esc=('http://' + _domain).replace('/', r'\\/'),
+        https_double_esc=('https://' + _domain).replace('/', r'\\/'),
+        # triple escape slash
+        slash_triple_esc=('//' + _domain).replace('/', r'\\\/'),
+        http_triple_esc=('http://' + _domain).replace('/', r'\\\/'),
+        https_triple_esc=('https://' + _domain).replace('/', r'\\\/'),
+        # urlencoded
+        slash_ue=quote_plus('//' + _domain),
+        http_ue=quote_plus('http://' + _domain),
+        https_ue=quote_plus('https://' + _domain),
+        double_quoted_ue=quote_plus('"%s"' % _domain),
+        single_quoted_ue=quote_plus("'%s'" % _domain),
+        # escaped and urlencoded
+        slash_esc_ue=quote_plus(s_esc('//' + _domain)),
+        http_esc_ue=quote_plus(s_esc('http://' + _domain)),
+        https_esc_ue=quote_plus(s_esc('https://' + _domain)),
+    )
+
+
 def add_temporary_domain_alias(source_domain, replaced_to_domain):
     """
     添加临时域名替换列表
@@ -468,6 +528,34 @@ def add_temporary_domain_alias(source_domain, replaced_to_domain):
     parse.temporary_domain_alias.append((source_domain, replaced_to_domain))
     dbgprint('A domain', source_domain, 'to', replaced_to_domain, 'added to temporary_domain_alias',
              parse.temporary_domain_alias)
+
+
+@lru_cache(maxsize=1024)
+def is_domain_match_glob_whitelist(domain):
+    """
+    域名是否匹配 `domains_whitelist_auto_add_glob_list` 中设置的通配符
+    :type domain: str
+    :rtype: bool
+    """
+    for domain_glob in domains_whitelist_auto_add_glob_list:
+        if fnmatch(domain, domain_glob):
+            return True
+    return False
+
+
+@lru_cache(maxsize=128)
+def is_content_type_streamed(_content_type):
+    """
+    根据content-type判断是否应该用stream模式传输(服务器下载的同时发送给用户)
+     视频/音频/图片等二进制内容默认用stream模式传输
+     :param _content_type: content_type, eg: "plain/text; encoding=utf-8"
+     :type _content_type: str
+     :rtype: bool
+    """
+    for streamed_keyword in steamed_mime_keywords:
+        if streamed_keyword in _content_type:
+            return True
+    return False
 
 
 # noinspection PyGlobalUndefined
@@ -509,6 +597,94 @@ def try_match_and_add_domain_to_rewrite_white_list(domain, force_add=False):
             traceback.print_exc()
 
         return True
+
+
+def current_line_number():
+    """Returns the current line number in our program.
+    :return: current line number
+    :rtype: int
+    """
+    import inspect
+    return inspect.currentframe().f_back.f_lineno
+
+
+@lru_cache(maxsize=1024)
+def extract_real_url_from_embedded_url(embedded_url):
+    """
+    将 embed_real_url_to_embedded_url() 编码后的url转换为原来的带有参数的url
+    `cdn_redirect_encode_query_str_into_url`设置依赖于本函数, 详细说明请看配置文件中这个参数的部分
+
+    eg: https://cdn.domain.com/a.php_zm24_.cT1zb21ldGhpbmc=._zm24_.css
+        ---> https://foo.com/a.php?q=something (assume it returns an css) (base64 only)
+    eg2: https://cdn.domain.com/a/b/_zm24_.bG92ZT1saXZl._zm24_.jpg
+        ---> https://foo.com/a/b/?love=live (assume it returns an jpg) (base64 only)
+    eg3: https://cdn.domain.com/a/b/_zm24z_.[some long long base64 encoded string]._zm24_.jpg
+        ---> https://foo.com/a/b/?love=live[and a long long query string] (assume it returns an jpg) (gzip + base64)
+    eg4:https://cdn.domain.com/a  (no change)
+        ---> (no query string): https://foo.com/a (assume it returns an png) (no change)
+    :param embedded_url: 可能被编码的URL
+    :return: 如果传入的是编码后的URL, 则返回解码后的URL, 否则返回None
+    :type embedded_url: str
+    :rtype: Union[str, None]
+    """
+    if '._' + cdn_url_query_encode_salt + '_.' not in embedded_url[-15:]:  # check url mark
+        return None
+    m = regex_extract_base64_from_embedded_url.search(embedded_url)
+    b64 = get_group('b64', m)
+    if not b64:
+        return None
+
+    # 'https://cdn.domain.com/a.php_zm24_.cT1zb21ldGhpbmc=._zm24_.css'
+    # real_request_url_no_query ---> 'https://cdn.domain.com/a.php'
+    real_request_url_no_query = embedded_url[:m.span()[0]]
+
+    query_string_byte = base64.urlsafe_b64decode(b64)
+    is_gzipped = get_group('gzip', m)
+    if is_gzipped:
+        query_string_byte = zlib.decompress(query_string_byte)
+    query_string = query_string_byte.decode(encoding='utf-8')
+
+    result = urljoin(real_request_url_no_query, '?' + query_string)
+    # dbgprint('extract:', embedded_url, 'to', result)
+    return result
+
+
+@lru_cache(maxsize=1024)
+def embed_real_url_to_embedded_url(real_url_raw, url_mime, escape_slash=False):
+    """
+    将url的参数(?q=some&foo=bar)编码到url路径中, 并在url末添加一个文件扩展名
+    在某些对url参数支持不好的CDN中, 可以减少错误
+    `cdn_redirect_encode_query_str_into_url`设置依赖于本函数, 详细说明可以看配置文件中的对应部分
+    解码由 extract_real_url_from_embedded_url() 函数进行, 对应的例子也请看这个函数
+    :rtype: str
+    """
+    # dbgprint(real_url_raw, url_mime, escape_slash)
+    if escape_slash:
+        real_url = real_url_raw.replace(r'\/', '/')
+    else:
+        real_url = real_url_raw
+    url_sp = urlsplit(real_url)
+    if not url_sp.query:  # no query, needn't rewrite
+        return real_url_raw
+
+    byte_query = url_sp.query.encode()
+    if len(byte_query) > 128:  # 当查询参数太长时, 进行gzip压缩
+        gzip_label = 'z'  # 进行压缩后的参数, 会在标识区中添加一个z
+        byte_query = zlib.compress(byte_query)
+    else:
+        gzip_label = ''
+
+    b64_query = base64.urlsafe_b64encode(byte_query).decode()
+    # dbgprint(url_mime)
+    mixed_path = url_sp.path + '_' + _url_salt + gzip_label + '_.' \
+                 + b64_query \
+                 + '._' + _url_salt + '_.' + mime_to_use_cdn[url_mime]
+    result = urlunsplit((url_sp.scheme, url_sp.netloc, mixed_path, '', ''))
+
+    if escape_slash:
+        result = s_esc(result)
+        # dbgprint('embed:', real_url_raw, 'to:', result)
+    return result
 
 
 def decode_mirror_url(mirror_url=None):
@@ -635,12 +811,88 @@ def is_target_domain_use_https(target_domain):
         return False
 
 
+def get_ext_domain_inurl_scheme_prefix(ext_domain, force_https=None):
+    """旧版本遗留函数, 已经不再需要, 永远返回空字符串"""
+    return ''
+
+
 def add_ssrf_allowed_domain(domain):
     """添加域名到ssrf白名单, 不支持通配符
     :type domain: str
     """
     global allowed_domains_set
     allowed_domains_set.add(domain)
+
+
+def strx(*args):
+    """
+    :return: str
+    """
+    output = ''
+    for arg in args:
+        output += str(arg) + ' '
+    output.rstrip(' ')
+    return output
+
+
+@lru_cache(maxsize=1024)
+def check_global_ua_pass(ua_str):
+    """该user-agent是否满足全局白名单"""
+    if ua_str is None or not global_ua_white_name:
+        return False
+    ua_str = ua_str.lower()
+    if global_ua_white_name in ua_str:
+        return True
+    else:
+        return False
+
+
+@lru_cache(maxsize=128)
+def is_mime_represents_text(input_mime):
+    """
+    Determine whether an mime is text (eg: text/html: True, image/png: False)
+    :param input_mime: str
+    :return: bool
+    """
+    input_mime_l = input_mime.lower()
+    for text_word in text_like_mime_keywords:
+        if text_word in input_mime_l:
+            return True
+    return False
+
+
+@lru_cache(maxsize=128)
+def extract_mime_from_content_type(_content_type):
+    """从content-type中提取出mime, 如 'text/html; encoding=utf-8' --> 'text/html'
+    :rtype: str
+    """
+    c = _content_type.find(';')
+    if c == -1:
+        return _content_type
+    else:
+        return _content_type[:c]
+
+
+@lru_cache(maxsize=128)
+def is_content_type_using_cdn(_content_type):
+    """根据content-type确定该资源是否使用CDN"""
+    _mime = extract_mime_from_content_type(_content_type)
+    if _mime in mime_to_use_cdn:
+        # dbgprint(content_type, 'Should Use CDN')
+        return _mime
+    else:
+        # dbgprint(content_type, 'Should NOT CDN')
+        return False
+
+
+def generate_simple_resp_page(errormsg=b'We Got An Unknown Error', error_code=500):
+    """
+
+    :type errormsg: bytes
+    :type error_code: int
+    :rtype: Response
+    """
+    return make_response(errormsg, error_code)
 
 
 def generate_error_page(errormsg='Unknown Error', error_code=500, is_traceback=False, content_only=False):
@@ -702,13 +954,42 @@ If you can't solve it by your self, here are some ways may help:<br>
 <a href="{official_site}" target="_blank">{official_site}</a></div>
 </body></html>""".format(errormsg=errormsg, request_detail=request_detail,
                          traceback_str=html_escape(traceback.format_exc()) if is_traceback else 'None or not displayed',
-                         version=CONSTS.__VERSION__, official_site=CONSTS.__GITHUB_URL__
+                         version=__VERSION__, official_site=__GITHUB_URL__
                          )
 
     if not content_only:
         return make_response(error_page.encode(), error_code)
     else:
         return error_page
+
+
+def generate_html_redirect_page(target_url, msg='', delay_sec=1):
+    """生成一个HTML重定向页面
+    某些浏览器在301/302页面不接受cookies, 所以需要用html重定向页面来传cookie
+    :type target_url: str
+    :type msg: str
+    :type delay_sec: int
+    :rtype: Response
+    """
+    resp_content = r"""<!doctype html>
+<html lang="zh-CN">
+<head>
+<meta charset="UTF-8">
+<title>重定向 (Page Redirect)</title>
+<meta http-equiv="refresh" content="%d; url=%s">
+<script>setTimeout(function(){location.href="%s"} , %d000);</script>
+</head>
+<body>
+<pre>%s</pre>
+<hr />
+You are now redirecting to <a href="%s">%s</a>, if it didn't redirect automatically, please click that link.
+</body>
+</html>""" % (
+        delay_sec, html_escape(target_url), html_escape(target_url), delay_sec + 1,
+        html_escape(msg), html_escape(target_url), html_escape(target_url)
+    )
+    resp_content = resp_content.encode('utf-8')
+    return Response(response=resp_content)
 
 
 def generate_304_response(_content_type=None):
@@ -798,7 +1079,7 @@ def put_response_to_local_cache(url, _our_resp, without_content=False):
     :type without_content: bool
     """
     # Only cache GET method, and only when remote returns 200(OK) status
-    if request.method != 'GET' or _our_resp.status_code != 200:
+    if request.method != 'GET' or parse.remote_response.status_code != 200:
         return
 
     dbgprint('PuttingCache:', url, "without_content:", without_content)
@@ -851,6 +1132,19 @@ def try_get_cached_response(url, client_header=None):
             return resp
     else:
         return None
+
+
+def get_group(name, match_obj):
+    """return a blank string if the match group is None"""
+    try:
+        obj = match_obj.group(name)
+    except:
+        return ''
+    else:
+        if obj is not None:
+            return obj
+        else:
+            return ''
 
 
 def regex_url_reassemble(match_obj):
@@ -1018,7 +1312,7 @@ def is_denied_because_of_spider(ua_str):
 def load_ip_whitelist_file():
     """从文件加载ip白名单"""
     set_buff = set()
-    if os.path.exists(zmirror_root(human_ip_verification_whitelist_file_path)):
+    if os.path.exists(human_ip_verification_whitelist_file_path):
         with open(zmirror_root(human_ip_verification_whitelist_file_path), 'r', encoding='utf-8') as fp:
             set_buff.add(fp.readline().strip())
     return set_buff
@@ -1699,7 +1993,7 @@ def request_remote_site_and_parse():
         resp.headers.add('X-Body-Req-Time', "%.4f" % req_time_body)
         resp.headers.add('X-Compute-Time', "%.4f" % (time() - parse.start_time - req_time_headers - req_time_body))
 
-    resp.headers.add('X-Powered-By', 'zmirror/%s' % CONSTS.__VERSION__)
+    resp.headers.add('X-Powered-By', 'zmirror/%s' % __VERSION__)
 
     if developer_dump_all_traffics and not is_streamed:
         if not os.path.exists(zmirror_root('traffic')):
@@ -2165,8 +2459,8 @@ Note: Love Luciaz Forever!
 
 Mirroring: {source_site}
 This site: {my_domain}
-""".format(version=CONSTS.__VERSION__, author=CONSTS.__AUTHOR__,
-           github_url=CONSTS.__GITHUB_URL__, source_site=target_domain,
+""".format(version=__VERSION__, author=__AUTHOR__,
+           github_url=__GITHUB_URL__, source_site=target_domain,
            my_domain=my_host_name),
                     content_type='text/plain')
 
