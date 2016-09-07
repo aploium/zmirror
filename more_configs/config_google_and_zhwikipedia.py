d710f05b4493316a283cf38de522de554987adf1
Aploium <i@z.codes>
2016-09-07 14:23:30 +0800

    [-] 移除google配置中过时的部分 (在0.28.3之后, 处理速度不会再随external_domains增多而线性下降)

--- more_configs/config_google_and_zhwikipedia.py
+++ more_configs/config_google_and_zhwikipedia.py
@@ -23,15 +23,6 @@
 # 以下服务暂不可用(因为目前无法解决登录的问题):
 #     所有需要登录的东西, docs之类的
 #
-# 不过, 因为试图反代整个google, 运算速度会慢一些
-#   本文件同时也提供了一个轻量版的配置, 请把所有最后面带有 '# 需要轻量级Google镜像请注释掉本行'
-#   轻量级配置文件中可用的google功能(完整可用, 没有瑕疵)为:
-#       Google搜索/学术/图片搜索/视频搜索/与中文维基百科无缝结合
-#   尽管是轻量级, 但是仍然是目前互联网上能找到的最好(功能/整合性/用户体验/访问速度)的Google镜像方案
-#
-# 速度对比, 在一台256M Ramnode OpenVZ VPS(Intel E3 3.3GHz)上处理Google首页(169.41KB), 测试5次取平均, 排除请求时间, 只测量运算时间
-#   全功能: 0.167秒
-#   轻量版: 0.045秒
 
 # Github: https://github.com/aploium/zmirror
 
@@ -75,105 +66,105 @@ external_domains = (
     'csi.gstatic.com',
     'fonts.googleapis.com',
 
-    # Google登陆支持, 因为现在登陆还bug多多, 注释掉它们也没关系
+    # Google登陆支持
     'accounts.google.com',
     'accounts.youtube.com',
     'accounts.google.com.hk',
     'myaccount.google.com',
     'myaccount.google.com.hk',
 
-    # # 需要轻量级Google镜像请注释掉以下的一堆域名...它们会拖慢内容重写速度
-    'ajax.googleapis.com',  # 需要轻量级Google镜像请注释掉本行
-    'translate.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'translate.google.com.hk',  # 需要轻量级Google镜像请注释掉本行
-    'video.google.com.hk',  # 需要轻量级Google镜像请注释掉本行
-    'books.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'cloud.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'analytics.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'security.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'investor.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'families.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'clients1.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'clients2.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'clients3.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'clients4.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'clients5.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'talkgadget.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'news.google.com.hk',  # 需要轻量级Google镜像请注释掉本行
-    'news.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'support.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'docs.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'books.google.com.hk',  # 需要轻量级Google镜像请注释掉本行
-    'chrome.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'profiles.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'feedburner.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'cse.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'sites.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'productforums.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'encrypted.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'm.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'research.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'maps.google.com.hk',  # 需要轻量级Google镜像请注释掉本行
-    'hangouts.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'developers.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'get.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'afp.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'groups.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'payments.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'photos.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'play.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'mail.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'code.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'tools.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'drive.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'script.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'goto.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'calendar.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'wallet.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'privacy.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'ipv4.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'video.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'store.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'fi.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'apps.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'events.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'notifications.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'plus.google.com',  # 需要轻量级Google镜像请注释掉本行
+    'ajax.googleapis.com',
+    'translate.google.com',
+    'translate.google.com.hk',
+    'video.google.com.hk',
+    'books.google.com',
+    'cloud.google.com',
+    'analytics.google.com',
+    'security.google.com',
+    'investor.google.com',
+    'families.google.com',
+    'clients1.google.com',
+    'clients2.google.com',
+    'clients3.google.com',
+    'clients4.google.com',
+    'clients5.google.com',
+    'talkgadget.google.com',
+    'news.google.com.hk',
+    'news.google.com',
+    'support.google.com',
+    'docs.google.com',
+    'books.google.com.hk',
+    'chrome.google.com',
+    'profiles.google.com',
+    'feedburner.google.com',
+    'cse.google.com',
+    'sites.google.com',
+    'productforums.google.com',
+    'encrypted.google.com',
+    'm.google.com',
+    'research.google.com',
+    'maps.google.com.hk',
+    'hangouts.google.com',
+    'developers.google.com',
+    'get.google.com',
+    'afp.google.com',
+    'groups.google.com',
+    'payments.google.com',
+    'photos.google.com',
+    'play.google.com',
+    'mail.google.com',
+    'code.google.com',
+    'tools.google.com',
+    'drive.google.com',
+    'script.google.com',
+    'goto.google.com',
+    'calendar.google.com',
+    'wallet.google.com',
+    'privacy.google.com',
+    'ipv4.google.com',
+    'video.google.com',
+    'store.google.com',
+    'fi.google.com',
+    'apps.google.com',
+    'events.google.com',
+    'notifications.google.com',
+    'plus.google.com',
 
-    'scholar.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
-    'translate.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
-    't0.gstatic.com',  # 需要轻量级Google镜像请注释掉本行
-    't1.gstatic.com',  # 需要轻量级Google镜像请注释掉本行
-    't2.gstatic.com',  # 需要轻量级Google镜像请注释掉本行
-    't3.gstatic.com',  # 需要轻量级Google镜像请注释掉本行
-    's-v6exp1-ds.metric.gstatic.com',  # 需要轻量级Google镜像请注释掉本行
-    'ci4.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
-    'gp3.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
-    'accounts.gstatic.com',  # 需要轻量级Google镜像请注释掉本行
+    'scholar.googleusercontent.com',
+    'translate.googleusercontent.com',
+    't0.gstatic.com',
+    't1.gstatic.com',
+    't2.gstatic.com',
+    't3.gstatic.com',
+    's-v6exp1-ds.metric.gstatic.com',
+    'ci4.googleusercontent.com',
+    'gp3.googleusercontent.com',
+    'accounts.gstatic.com',
 
     # For Google Map (optional)
-    'maps-api-ssl.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'maps.gstatic.com',  # 需要轻量级Google镜像请注释掉本行
-    'maps.google.com',  # 需要轻量级Google镜像请注释掉本行
-    'fonts.gstatic.com',  # 需要轻量级Google镜像请注释掉本行
-    'lh1.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
-    'lh2.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
-    'lh3.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
-    'lh4.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
-    'lh5.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
-    'lh6.googleusercontent.com',  # 需要轻量级Google镜像请注释掉本行
+    'maps-api-ssl.google.com',
+    'maps.gstatic.com',
+    'maps.google.com',
+    'fonts.gstatic.com',
+    'lh1.googleusercontent.com',
+    'lh2.googleusercontent.com',
+    'lh3.googleusercontent.com',
+    'lh4.googleusercontent.com',
+    'lh5.googleusercontent.com',
+    'lh6.googleusercontent.com',
 
     # 'upload.wikimedia.org',
-    'id.google.com.hk',  # 需要轻量级Google镜像请注释掉本行
-    'id.google.com',  # 需要轻量级Google镜像请注释掉本行
+    'id.google.com.hk',
+    'id.google.com',
 )
 
+# 强制所有Google站点使用HTTPS
 force_https_domains = 'ALL'
 
-# 需要轻量级Google的请一定要注释掉下面这两行, 否则会动态添加大量的域名, 导致很快就变慢(支持的Google服务也变多)
-enable_automatic_domains_whitelist = True  # 需要轻量级Google镜像请注释掉本行
+# 自动动态添加域名
+enable_automatic_domains_whitelist = True
 domains_whitelist_auto_add_glob_list = (
-    '*.google.com', '*.gstatic.com', '*.google.com.hk', '*.googleapis.com')  # 需要轻量级Google镜像请注释掉本行
+    '*.google.com', '*.gstatic.com', '*.google.com.hk', '*.googleapis.com')
 
 # ############## Proxy Settings ##############
 # 如果你在墙内使用本配置文件, 请指定一个墙外的http代理
