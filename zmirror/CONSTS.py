7841ce81c0194bc7bef8a261c188a6a391ff09c4
Aploium <i@z.codes>
2016-09-21 16:29:44 +0800

    [*] 版本号改为 0.29.2

--- zmirror/CONSTS.py
+++ zmirror/CONSTS.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import os
 
-__VERSION_TUPLE__ = (0, 29, 1, "")
+__VERSION_TUPLE__ = (0, 29, 2, "")
 __VERSION__ = ".".join(str(x) for x in __VERSION_TUPLE__).rstrip(".")
 __AUTHOR__ = 'Aploium <i@z.codes>'
 __GITHUB_URL__ = 'https://github.com/aploium/zmirror'
