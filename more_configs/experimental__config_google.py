197be3e619995fdaa1ab2b71f38ce7453ace7c88
Aploium <i@z.codes>
2016-09-24 08:24:24 +0800

    [+] 添加一个实验性的 www.google.com 配置文件, 该配置文件的镜像是默认关闭"安全搜索"的 #10

---
+++ more_configs/experimental__config_google.py
@@ -0,0 +1,253 @@
+# coding=utf-8
+# 这是为Google和中文维基(无缝整合)镜像配置的示例配置文件
+#
+# 本文件为实验性配置文件. 传统google配置文件镜像的是 www.google.com.hk
+#   而本文件镜像的是 www.google.com, 区别在于前者不允许关闭"安全搜索"而后者默认关闭
+#
+# 使用方法:
+#   1. 复制本文件到 zmirror 根目录(wsgi.py所在目录), 并重命名为 config.py
+#   2. 复制 experimental__custom_func_google.py 到zmirror根目录, 并重命名为 custom_func.py
+#   2. 修改 my_host_name 为你自己的域名
+#
+#
+# 警告: 本文件为实验性配置文件, 正常情况下请使用 config_google_and_zhwikipedia.py 这个配置文件
+# 警告: 本文件为实验性配置文件, 正常情况下请使用 config_google_and_zhwikipedia.py 这个配置文件
+# 警告: 本文件为实验性配置文件, 正常情况下请使用 config_google_and_zhwikipedia.py 这个配置文件
+# 警告: 本文件为实验性配置文件, 正常情况下请使用 config_google_and_zhwikipedia.py 这个配置文件
+#
+#
+# 各项设置选项的详细介绍请看 config_default.py 中对应的部分
+# 本配置文件假定你的服务器本身在墙外
+# 如果服务器本身在墙内(或者在本地环境下测试, 请修改`Proxy Settings`中的设置
+#
+# 由于google搜索结果经常会出现中文维基, 所以顺便把它也加入了.
+# google跟中文维基之间使用了本程序的镜像隔离功能, 可以保证中文维基站的正常使用
+#
+# 本配置文件试图还原出一个功能完整的google.
+#   但是由于程序本身所限, 还是不能[完整]镜像过来整个[google站群]
+#   在后续版本会不断增加可用的网站
+#
+# 以下google服务完全可用:
+#   google网页搜索/学术/图片/新闻/图书/视频(搜索)/财经/APP搜索/翻译/网页快照/...
+#   google搜索与中文维基百科无缝结合
+# 以下服务部分可用:
+#     gg地图(地图可看, 左边栏显示不正常)/G+(不能登录)
+# 以下服务暂不可用(因为目前无法解决登录的问题):
+#     所有需要登录的东西, docs之类的
+#
+
+# Github: https://github.com/aploium/zmirror
+
+# ############## Local Domain Settings ##############
+my_host_name = '127.0.0.1'
+my_host_scheme = 'http://'
+my_host_port = None  # None表示使用默认端口, 可以设置成非标准端口, 比如 81
+
+# ############## Target Domain Settings ##############
+target_domain = 'www.google.com'
+target_scheme = 'https://'
+
+# 这里面大部分域名都是通过 `enable_automatic_domains_whitelist` 自动采集的, 我只是把它们复制黏贴到了这里
+# 实际镜像一个新的站时, 手动只需要添加很少的几个域名就可以了.
+# 自动采集(如果开启的话)会不断告诉你新域名
+external_domains = (
+    'webcache.googleusercontent.com',  # Google网页快照
+    'images.google.com',
+    'apis.google.com',
+
+    # Google学术
+    'scholar.google.com',
+
+    # 中文维基百科
+    'zh.wikipedia.org',
+    'zh.m.wikipedia.org',
+    'upload.wikipedia.org',
+    'meta.wikimedia.org',
+    'login.wikimedia.org',
+
+    # Google静态资源域名
+    'ssl.gstatic.com',
+    'www.gstatic.com',
+    'encrypted-tbn0.gstatic.com',
+    'encrypted-tbn1.gstatic.com',
+    'encrypted-tbn2.gstatic.com',
+    'encrypted-tbn3.gstatic.com',
+    'csi.gstatic.com',
+    'fonts.googleapis.com',
+
+    # Google登陆支持
+    'accounts.google.com',
+    'accounts.youtube.com',
+    'myaccount.google.com',
+
+    'ajax.googleapis.com',
+    'translate.google.com',
+    'books.google.com',
+    'cloud.google.com',
+    'analytics.google.com',
+    'security.google.com',
+    'investor.google.com',
+    'families.google.com',
+    'clients1.google.com',
+    'clients2.google.com',
+    'clients3.google.com',
+    'clients4.google.com',
+    'clients5.google.com',
+    'talkgadget.google.com',
+    'news.google.com',
+    'support.google.com',
+    'docs.google.com',
+    'chrome.google.com',
+    'profiles.google.com',
+    'feedburner.google.com',
+    'cse.google.com',
+    'sites.google.com',
+    'productforums.google.com',
+    'encrypted.google.com',
+    'm.google.com',
+    'research.google.com',
+    'hangouts.google.com',
+    'developers.google.com',
+    'get.google.com',
+    'afp.google.com',
+    'groups.google.com',
+    'payments.google.com',
+    'photos.google.com',
+    'play.google.com',
+    'mail.google.com',
+    'code.google.com',
+    'tools.google.com',
+    'drive.google.com',
+    'script.google.com',
+    'goto.google.com',
+    'calendar.google.com',
+    'wallet.google.com',
+    'privacy.google.com',
+    'ipv4.google.com',
+    'video.google.com',
+    'store.google.com',
+    'fi.google.com',
+    'apps.google.com',
+    'events.google.com',
+    'notifications.google.com',
+    'plus.google.com',
+    'dl.google.com',
+    'manifest.googlevideo.com',
+    'storage.googleapis.com',
+
+    'gg.google.com',
+
+    'scholar.googleusercontent.com',
+    'translate.googleusercontent.com',
+    't0.gstatic.com',
+    't1.gstatic.com',
+    't2.gstatic.com',
+    't3.gstatic.com',
+    's-v6exp1-ds.metric.gstatic.com',
+    'ci4.googleusercontent.com',
+    'gp3.googleusercontent.com',
+    'accounts.gstatic.com',
+
+    # For Google Map (optional)
+    'maps-api-ssl.google.com',
+    'maps.gstatic.com',
+    'maps.google.com',
+    'fonts.gstatic.com',
+    'lh1.googleusercontent.com',
+    'lh2.googleusercontent.com',
+    'lh3.googleusercontent.com',
+    'lh4.googleusercontent.com',
+    'lh5.googleusercontent.com',
+    'lh6.googleusercontent.com',
+
+    # 'upload.wikimedia.org',
+    'id.google.com',
+
+    # misc
+    'inputtools.google.com',
+    'inbox.google.com',
+    '-v6exp3-v4.metric.gstatic.com',
+    '-v6exp3-ds.metric.gstatic.com',
+    'if-v6exp3-v4.metric.gstatic.com',
+    'public.talk.google.com',
+    'ie.talkgadget.google.com',
+    'client-channel.google.com',
+    'maps.googleapis.com',
+    'people-pa.clients6.google.com',
+    'myphonenumbers-pa.googleapis.com',
+    'clients6.google.com',
+    'staging.talkgadget.google.com',
+    'preprod.hangouts.sandbox.google.com',
+    'dev-hangoutssearch-pa-googleapis.sandbox.google.com',
+    'picasaweb.google.com',
+    'schemas.google.com',
+    'contact.talk.google.com',
+    'groupchat.google.com',
+    'friendconnectchat.google.com',
+    'muvc.google.com',
+    'bot.talk.google.com',
+    'prom.corp.google.com',
+    'stun.l.google.com',
+    'stun1.l.google.com',
+    'stun2.l.google.com',
+    'stun3.l.google.com',
+    'stun4.l.google.com',
+    'onetoday.google.com',
+    'plus.googleapis.com',
+    'youtube.googleapis.com',
+    'picasa.google.com',
+
+    'plus.sandbox.google.com',
+
+    # gmail misc
+    'gmail.com',
+    'www.gmail.com',
+    'chatenabled.mail.google.com',
+    'filetransferenabled.mail.google.com',
+    'gmail.google.com',
+    'googlemail.l.google.com',
+    'isolated.mail.google.com',
+    'm.gmail.com',
+    'm.googlemail.com',
+    'mail-settings.google.com',
+    'm.android.com',
+)
+
+# 强制所有Google站点使用HTTPS
+force_https_domains = 'ALL'
+
+# 自动动态添加域名
+enable_automatic_domains_whitelist = True
+domains_whitelist_auto_add_glob_list = (
+    '*.google.com', '*.gstatic.com', '*.googleapis.com')
+
+# ############## Sites Isolation ##############
+enable_individual_sites_isolation = True
+
+# 镜像隔离, 用于支持Google和维基共存
+isolated_domains = {'zh.wikipedia.org', 'zh.m.wikipedia.org'}
+
+# ############## URL Custom Redirect ##############
+url_custom_redirect_enable = True
+url_custom_redirect_list = {
+    # 这是一个方便的设置, 如果你访问 /wiki ,程序会自动重定向到后面这个长长的wiki首页
+    '/wiki': '/extdomains/zh.wikipedia.org/',
+
+    # 这是gmail
+    '/gmail': '/extdomains/mail.google.com/mail/u/0/h/girbaeneuj90/',
+
+    # 这是google学术
+    "/scholar": '/extdomains/scholar.google.com/?hl=zh-CN',
+}
+
+# ############# Custom Redirection ##############
+custom_prior_request_redirect_enable = True
+
+# ############## Proxy Settings ##############
+# 如果你在墙内使用本配置文件, 请指定一个墙外的http代理
+is_use_proxy = False
+# 代理的格式及SOCKS代理, 请看 http://docs.python-requests.org/en/latest/user/advanced/#proxies
+requests_proxies = dict(
+    http='http://127.0.0.1:8123',
+    https='https://127.0.0.1:8123',
+)
---
+++ more_configs/experimental__custom_func_google.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+try:
+    from typing import Union
+except:
+    pass
+from flask import Response, Request
+from zmirror.zmirror import ZmirrorThreadLocal
+
+
+def custom_prior_redirect_func(request, parse):
+    """
+    用于在 prior_request_redirect 阶段的自定义重定向
+
+    若返回一个 flask.Response 对象, 则执行重定向, 直接返回这个 Response
+    若返回None, 则不进行重定向
+
+    不应该修改parse变量 (添加头和cookie除外)
+
+    详见 `config_default.py` 中 `Custom Redirection` 部分
+    ------------------------------
+
+    对于本函数, 其功能是检查用户cookies中是否出现了 zmirror_ncr
+      如果已出现, 则表示已经访问过了 https://www.google.com/ncr 进行了国别重定向避免.
+        则不做任何事
+      否则则需要重定向到 /ncr 进行一次请求, 来避免国别重定向
+
+    由于国别重定向以后的语言会变成英语, 所以还需要将语言修改为简中
+      修改方式是在 query string 中加入 hl=zh-CN
+      本函数的另一个作用就是在 query string 中加入 hl=zh-CN
+
+    对于google镜像的国别重定向问题, 请看 issues#10
+      https://github.com/aploium/zmirror/issues/10
+
+    感谢 @licess 指出的这个问题
+
+    :param request: flask request object
+    :type request: Request
+    :param parse: the zmirror parse variable
+    :type parse: ZmirrorThreadLocal
+    :rtype: Union[Response, None]
+    """
+    from urllib.parse import urlsplit, urlunsplit
+    from flask import redirect
+
+    zmirror_ncr = request.cookies.get("zmirror_ncr")
+
+    if zmirror_ncr == "y" and parse.remote_path in ("/", "/webhp", "/search"):
+        # 如果在cookies中已经设置了zmirror的ncr标记 并且请求的是这三个path
+        # 则将搜索语言修改为中文(通过在query string中添加 hl=zh-CN 来实现)
+        if "hl=" in parse.remote_path_query:  # 如果已经指定了 hl= 则跳过
+            return None
+
+        else:
+            # 否则通过重定向在 query string 中加入 hl=zh-CN
+            sp = list(urlsplit(request.url))
+            if sp[3] == "":
+                sp[3] = "hl=zh-CN"
+            else:
+                sp[3] += "&hl=zh-CN"
+            return redirect(urlunsplit(sp), code=307)
+
+    elif parse.remote_path in ("/", "/webhp"):
+        # 仅当 prepared 以后的下一次访问 google首页才触发 /ncr 重定向
+        #   ps: /webhp 等效于 /
+        return redirect("/ncr", code=302)
+
+    elif parse.remote_path == "/ncr":
+        # 如果正在请求 /ncr , 则在cookie中设置 zmirror_ncr=y (不作重定向)
+        # "y" 代表 yes
+        parse.set_cookies("zmirror_ncr", "y")  # TTL是一年
+        return None
+
+    else:
+        # 其他情况什么都不做
+        return None
