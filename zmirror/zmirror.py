5fc09047d77be7412e92ceb2426c71ebbde97aee
Aploium <i@z.codes>
2016-10-06 14:39:24 +0800

    [!*] (部分完成)把正则部分移动到regex模块 [*] 将parse变量从 zmirror.py 移动到 threadlocal.py 以便其他代码从中引用 #12

--- zmirror/zmirror.py
+++ zmirror/zmirror.py
@@ -14,7 +14,6 @@ import ipaddress
 import threading
 import importlib
 
-from fnmatch import fnmatch
 from time import time, sleep, process_time
 from html import escape as html_escape
 from datetime import datetime, timedelta
@@ -62,8 +61,6 @@ else:
     if not cfg.unittest_mode:
         infoprint('lru_cache loaded successfully from fastcache')
 
-from .threadlocal import ZmirrorThreadLocal
-
 if cfg.unittest_mode:
     importlib.reload(importlib.import_module("zmirror.utils_simple"))
     importlib.reload(importlib.import_module("zmirror.utils_complex"))
@@ -72,7 +69,9 @@ if cfg.unittest_mode:
 from .utils_simple import *
 from .utils_complex import *
 from .lru_dict import LRUDict
+from .threadlocal import parse
 from . import connection_pool
+from . import regex
 
 if cfg.local_cache_enable:
     try:
@@ -96,9 +95,6 @@ if cfg.developer_enable_experimental_feature:  # coverage: exclude
     pass
 
 # ########### Global Variables ###############
-# 与flask的request变量功能类似, 存储了一些解析后的请求信息, 在程序中会经常被调用
-parse = ZmirrorThreadLocal()
-
 # task_scheduler
 task_scheduler = sched.scheduler(time, sleep)
 
@@ -121,140 +117,6 @@ recent_domains[cfg.target_domain] = True
 domain_guess_cache = LRUDict(1000)
 # 格式如下:
 domain_guess_cache[("example.com", "/path/no/query/string")] = "target.domain.com"
-
-# ########### PreCompile Regex ###############
-
-# 冒号(colon :)可能的值为:
-#    : %3A %253A  完整列表见 tests.TestRegex.REGEX_POSSIBLE_COLON
-REGEX_COLON = r"""(?::|%(?:25)?3[Aa])"""
-# 斜线(slash /)可能的值为(包括大小写):
-# 完整列表见 tests.TestRegex.REGEX_POSSIBLE_COLON
-#    / \/ \\/ \\\(N个反斜线)/ %2F %5C%2F %5C%5C(N个5C)%2F %255C%252F %255C%255C%252F \x2F
-REGEX_SLASH = r"""(?:\\*(?:/|x2[Ff])|%(?:(?:25)?5[Cc]%)*(?:25)?2[Ff])"""
-# 引号 可能值的完整列表见 tests.TestRegex.REGEX_POSSIBLE_QUOTE
-# " ' \\(可能有N个反斜线)' \\(可能有N个反斜线)"
-# %22 %27 %5C(可能N个5C)%22 %5C(可能N个5C)%27
-# %2522 %2527 %255C%2522 %255C%2527
-# &quot;
-REGEX_QUOTE = r"""(?:\\*["']|%(?:(?:25)?5[Cc]%)*2(?:52)?[27]|&quot;)"""
-
-# 代表本镜像域名的正则
-if cfg.my_host_port is not None:
-    REGEX_MY_HOST_NAME = r'(?:' + re.escape(cfg.my_host_name_no_port) + REGEX_COLON + re.escape(str(cfg.my_host_port)) \
-                         + r'|' + re.escape(cfg.my_host_name_no_port) + r')'
-else:
-    REGEX_MY_HOST_NAME = re.escape(cfg.my_host_name)
-
-# Advanced url rewriter, see function response_text_rewrite()
-# #### 这个正则表达式是整个程序的最核心的部分, 它的作用是从 html/css/js 中提取出长得类似于url的东西 ####
-# 如果需要阅读这个表达式, 请一定要在IDE(如PyCharm)的正则高亮下阅读
-# 这个正则并不保证匹配到的东西一定是url, 在 regex_url_reassemble() 中会进行进一步验证是否是url
-regex_adv_url_rewriter = re.compile(
-    # 前缀, 必须有  'action='(表单) 'href='(链接) 'src=' 'url('(css) '@import'(css) '":'(js/json, "key":"value")
-    # \s 表示空白字符,如空格tab
-    r"""(?P<prefix>\b(?:(?:src|href|action)\s*=|url\s*\(|@import\s*|"\s*:)\s*)""" +  # prefix, eg: src=
-    # 左边引号, 可选 (因为url()允许没有引号). 如果是url以外的, 必须有引号且左右相等(在重写函数中判断, 写在正则里可读性太差)
-    r"""(?P<quote_left>["'])?""" +  # quote  "'
-    # 域名和协议头, 可选. http:// https:// // http:\/\/ (json) https:\/\/ (json) \/\/ (json)
-    r"""(?P<domain_and_scheme>(?P<scheme>(?:https?:)?\\?/\\?/)(?P<domain>(?:[-a-z0-9]+\.)+[a-z]+(?P<port>:\d{1,5})?))?""" +
-    # url路径, 含参数 可选
-    r"""(?P<path>[^\s;+$?#'"\{}]*?""" +  # full path(with query string)  /foo/bar.js?love=luciaZ
-    # 查询字符串, 可选
-    r"""(?P<query_string>\?[^\s?#'"]*?)?)""" +  # query string  ?love=luciaZ
-    # 右引号(可以是右括弧), 必须
-    r"""(?P<quote_right>["')])(?P<right_suffix>\W)""",  # right quote  "'
-    flags=re.IGNORECASE
-)
-
-# Response Cookies Rewriter, see response_cookie_rewrite()
-regex_cookie_rewriter = re.compile(r'\bdomain=(\.?([\w-]+\.)+\w+)\b', flags=re.IGNORECASE)
-regex_cookie_path_rewriter = re.compile(r'(?P<prefix>[pP]ath)=(?P<path>[\w\._/-]+?;)')
-
-# Request Domains Rewriter, see client_requests_text_rewrite()
-# 该正则用于匹配类似于下面的东西
-#   [[[http(s):]//]www.mydomain.com/]extdomains/(https-)target.com
-# 兼容各种urlencode/escape
-#
-# 注意, 若想阅读下面的正则表达式, 请一定要在 Pycharm 的正则高亮下进行
-# 否则不对可能的头晕/恶心负责
-# 下面那个正则, 在组装以后的样子大概是这样的(已大幅简化):
-# 假设b.test.com是本机域名
-#   ((https?:/{2})?b\.test\.com/)?extdomains/(https-)?((?:[\w-]+\.)+\w+)\b
-#
-# 对应的 unittest 见 TestRegex.test__regex_request_rewriter_extdomains()
-regex_request_rewriter_extdomains = re.compile(
-    r"""(?P<domain_prefix>""" +
-    (  # [[[http(s):]//]www.mydomain.com/]
-        r"""(?P<scheme>""" +
-        (  # [[http(s):]//]
-            (  # [http(s):]
-                r"""(?:https?(?P<colon>{REGEX_COLON}))?""".format(REGEX_COLON=REGEX_COLON)  # https?:
-            ) +
-            r"""(?P<scheme_slash>%s)(?P=scheme_slash)""" % REGEX_SLASH  # //
-        ) +
-        r""")?""" +
-        REGEX_MY_HOST_NAME +  # www.mydomain.com[:port] 本部分的正则在上面单独组装
-        r"""(?P<slash2>(?(scheme_slash)(?P=scheme_slash)|{REGEX_SLASH}))""".format(REGEX_SLASH=REGEX_SLASH)  # # /
-    ) +
-    r""")?""" +
-
-    r"""extdomains(?(slash2)(?P=slash2)|{REGEX_SLASH})(?P<is_https>https-)?""".format(
-        REGEX_SLASH=REGEX_SLASH) +  # extdomains/(https-)
-    r"""(?P<real_domain>(?:[\w-]+\.)+\w+)\b""",  # target.com
-    flags=re.IGNORECASE,
-)
-regex_request_rewriter_main_domain = re.compile(REGEX_MY_HOST_NAME)
-
-
-# 以下正则为*实验性*的 response_text_basic_rewrite() 的替代品
-# 用于函数 response_text_basic_mirrorlization()
-# 理论上, 在大量域名的情况下, 会比现有的暴力字符串替换要快, 并且未来可以更强大的域名通配符
-# v0.28.0加入, v0.28.3后默认启用
-def _regex_generate__basic_mirrorlization():
-    """产生 regex_basic_mirrorlization
-    用一个函数包裹起来是因为在 try_match_and_add_domain_to_rewrite_white_list()
-    中需要动态修改 external_domains, 修改以后可能需要随之生成新的正则, 包裹一下比较容易调用
-    """
-    from collections import Counter
-
-    # 统计各个后缀出现的频率, 并且按照出现频率降序排列, 有助于提升正则效率
-    c = Counter(re.escape(x.split(".")[-1]) for x in cfg.allowed_domains_set)
-    regex_all_remote_tld = sorted(list(c.keys()), key=lambda x: c[x], reverse=True)
-
-    regex_all_remote_tld = "(?:" + "|".join(regex_all_remote_tld) + ")"
-    return re.compile(
-        r"""(?:""" +
-        (  # [[http(s):]//] or [\?["']] or %27 %22 or &quot;
-            r"""(?P<scheme>""" +
-            (  # [[http(s):]//]
-                (  # [http(s):]
-                    r"""(?:https?(?P<colon>{REGEX_COLON}))?""".format(REGEX_COLON=REGEX_COLON)  # https?:
-                ) +
-                r"""(?P<scheme_slash>%s)(?P=scheme_slash)""" % REGEX_SLASH  # //
-            ) +
-            r""")""" +
-            r"""|""" +
-            # [\?["']] or %27 %22 or &quot
-            r"""(?P<quote>{REGEX_QUOTE})""".format(REGEX_QUOTE=REGEX_QUOTE)
-        ) +
-        r""")""" +
-        # End prefix.
-        # Begin domain
-        r"""(?P<domain>([a-zA-Z0-9-]+\.){1,5}%s)\b""" % regex_all_remote_tld +
-        # Optional suffix slash
-        r"""(?P<suffix_slash>(?(scheme_slash)(?P=scheme_slash)|{SLASH}))?""".format(SLASH=REGEX_SLASH) +
-
-        # right quote (if we have left quote)
-        r"""(?(quote)(?P=quote))"""
-    )
-
-
-regex_basic_mirrorlization = _regex_generate__basic_mirrorlization()
-
-# 用于移除掉cookie中类似于 zmirror_verify=75bf23086a541e1f; 的部分
-regex_remove__zmirror_verify__header = re.compile(
-    r"""zmirror_verify=[a-zA-Z0-9]+\b;? ?"""
-)
 
 # ########## Flask app ###########
 
@@ -319,7 +181,7 @@ def response_text_basic_mirrorlization(text):
             else:  # //target.domain
                 return slash * 2 + core
 
-    return regex_basic_mirrorlization.sub(regex_reassemble, text)
+    return regex.regex_basic_mirrorlization.sub(regex_reassemble, text)
 
 
 def encoding_detect(byte_content):
@@ -479,7 +341,6 @@ def try_match_and_add_domain_to_rewrite_white_list(domain, force_add=False):
     :type force_add: bool
     :rtype: bool
     """
-    global regex_basic_mirrorlization
 
     if domain is None or not domain:
         return False
@@ -499,7 +360,7 @@ def try_match_and_add_domain_to_rewrite_white_list(domain, force_add=False):
     prefix_buff[domain] = calc_domain_replace_prefix(domain)
 
     # 重新生成匹配正则
-    regex_basic_mirrorlization = _regex_generate__basic_mirrorlization()
+    regex.regex_basic_mirrorlization = regex.regex_generate__basic_mirrorlization()
 
     # write log
     try:
@@ -1268,13 +1129,13 @@ def response_cookies_deep_copy():
             if 'httponly' in value.lower():
                 if cfg.enable_aggressive_cookies_path_rewrite:
                     # 暴力cookie path重写, 把所有path都重写为 /
-                    value = regex_cookie_path_rewriter.sub('path=/;', value)
+                    value = regex.regex_cookie_path_rewriter.sub('path=/;', value)
                 elif cfg.enable_aggressive_cookies_path_rewrite is not None:
                     # 重写HttpOnly Cookies的path到当前url下
                     # eg(/extdomains/a.foobar.com): path=/verify; -> path=/extdomains/a.foobar.com/verify
 
                     if parse.remote_domain not in cfg.domain_alias_to_target_set:  # do not rewrite main domains
-                        value = regex_cookie_path_rewriter.sub(
+                        value = regex.regex_cookie_path_rewriter.sub(
                             '\g<prefix>=/extdomains/' + parse.remote_domain + '\g<path>', value)
 
             header_cookies_string_list.append(value)
@@ -1371,7 +1232,7 @@ def response_text_rewrite(resp_text):
             resp_text = resp_text.replace(before_replace, after_replace)
 
     # v0.9.2+: advanced url rewrite engine
-    resp_text = regex_adv_url_rewriter.sub(regex_url_reassemble, resp_text)
+    resp_text = regex.regex_adv_url_rewriter.sub(regex_url_reassemble, resp_text)
 
     if cfg.developer_string_trace is not None and cfg.developer_string_trace in resp_text:
         # debug用代码, 对正常运行无任何作用
@@ -1405,7 +1266,7 @@ def response_cookie_rewrite(cookie_string):
     rewrite response cookie string's domain to `my_host_name`
     :type cookie_string: str
     """
-    cookie_string = regex_cookie_rewriter.sub('domain=' + cfg.my_host_name_no_port, cookie_string)
+    cookie_string = regex.regex_cookie_rewriter.sub('domain=' + cfg.my_host_name_no_port, cookie_string)
     return cookie_string
 
 
@@ -1496,7 +1357,7 @@ def extract_client_header():
 
             # 移除掉 cookie 中的 zmirror_verify
             if head_name_l == "cookie":
-                rewrited_headers[head_name_l] = regex_remove__zmirror_verify__header.sub(
+                rewrited_headers[head_name_l] = regex.regex_remove__zmirror_verify__header.sub(
                     "",
                     rewrited_headers[head_name_l],
                 )
@@ -1544,14 +1405,14 @@ def client_requests_text_rewrite(raw_text):
 
     # 使用一个复杂的正则进行替换, 这次替换以后, 理论上所有 extdomains 都会被剔除
     # 详见本文件顶部, regex_request_rewriter_extdomains 本体
-    replaced = regex_request_rewriter_extdomains.sub(replace_to_real_domain, raw_text)
+    replaced = regex.regex_request_rewriter_extdomains.sub(replace_to_real_domain, raw_text)
 
     if cfg.developer_string_trace is not None and cfg.developer_string_trace in replaced:
         # debug用代码, 对正常运行无任何作用
         infoprint('StringTrace: appears client_requests_text_rewrite, code line no. ', current_line_number())
 
     # 正则替换掉单独的, 不含 /extdomains/ 的主域名
-    replaced = regex_request_rewriter_main_domain.sub(cfg.target_domain, replaced)
+    replaced = regex.regex_request_rewriter_main_domain.sub(cfg.target_domain, replaced)
 
     # 为了保险起见, 再进行一次裸的替换
     replaced = replaced.replace(cfg.my_host_name, cfg.target_domain)
